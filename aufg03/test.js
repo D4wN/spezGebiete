11afc9ea0da2480d5cfb66a6e302c53b2dc42208
Marv <marvin.lutz@fh-bielefeld.de>
2015-05-06 13:47:53 +0200

    final commit

--- aufg03/test.js
+++ aufg03/test.js
@@ -2,9 +2,11 @@
  * Created by Merlen on 03.05.2015.
  */
 
-var Converter = (function() {
-    function Converter() {}
-    Converter.prototype.rgbToHex = function(red, green, blue) {
+var Converter = (function () {
+    function Converter() {
+    }
+
+    Converter.prototype.rgbToHex = function (red, green, blue) {
         var redHex = red.toString(16);
         var greenHex = green.toString(16);
         var blueHex = blue.toString(16);
@@ -13,7 +15,8 @@ var Converter = (function() {
     function pad(hex) {
         return (hex.length === 1 ? "0" + hex : hex);
     }
-    Converter.prototype.hexToRgb = function(hex) {
+
+    Converter.prototype.hexToRgb = function (hex) {
         var red = parseInt(hex.substring(0, 2), 16);
         var green = parseInt(hex.substring(2, 4), 16);
         var blue = parseInt(hex.substring(4, 6), 16);
@@ -25,26 +28,26 @@ var Converter = (function() {
 
 var assert = require("assert");
 
-describe('Color Converter', function(){
-    describe('HEX to RGB', function(){
-        it('Input #ffeeff should be RGB[255,238,255]', function(){
+describe('Color Converter', function () {
+    describe('HEX to RGB', function () {
+        it('Input #ffeeff should be RGB[255,238,255]', function () {
             var c = new Converter();
             //http://stackoverflow.com/questions/13225274/the-difference-between-assert-equal-and-assert-deepequal-in-javascript-testing-w
-            assert.deepEqual(c.hexToRgb("ffeeff"), [255,238,255]);
+            assert.deepEqual(c.hexToRgb("ffeeff"), [255, 238, 255]);
         }),
-            it('Input #000000 should be RGB[0,0,0]', function(){
+            it('Input #000000 should be RGB[0,0,0]', function () {
                 var c = new Converter();
                 //http://stackoverflow.com/questions/13225274/the-difference-between-assert-equal-and-assert-deepequal-in-javascript-testing-w
-                assert.deepEqual(c.hexToRgb("000000"), [0,0,0]);
+                assert.deepEqual(c.hexToRgb("000000"), [0, 0, 0]);
             })
     });
 
-    describe('RGB to HEX', function(){
-        it('Input RGB[244,255,244] should return #f4fff4', function(){
+    describe('RGB to HEX', function () {
+        it('Input RGB[244,255,244] should return #f4fff4', function () {
             var c = new Converter();
             assert.equal(c.rgbToHex(244, 255, 244), "f4fff4");
         }),
-            it('Input RGB[0 0,0] should return #000000', function(){
+            it('Input RGB[0 0,0] should return #000000', function () {
                 var c = new Converter();
                 assert.equal(c.rgbToHex(0, 0, 0), "000000");
             })
