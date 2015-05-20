50d7db634fa7b5ae6dd7980d78cd31ada48bab84
Marv <marvin.lutz@fh-bielefeld.de>
2015-05-20 10:31:35 +0200

    querie fix

--- aufgaben/queries_2.js
+++ aufgaben/queries_2.js
@@ -4,20 +4,34 @@
 
 
 //1. Wie lauten die eindeutigen Namen der „Folder“ der E-Mails?
-db.mail.find({},{
-    fpatch: 1
-})
-//oder NAME
-db.mail.find({},{
-    fname: 1
-})
+db.mail.aggregate([{
+    $group :{
+        _id :"$folder"
+    }
+}])
 //2. Wieviel E-Mails wurden mit der Adresse “rosalee.fleming@enron.com“ gesendet?
 db.mail.count({
     sender: "rosalee.fleming@enron.com"
 })
 
 //3. Was ist die E-Mail mit der höchsten Anzahl an Rezipienten und was ist der Nachrichtentext der E-Mail?
-db.mail.find({},{text:1}).sort({recipients: -1}).limit(1)
+db.mail.aggregate(
+    [
+        {
+            $project: {
+                message: "$text",
+                datum: "$date",
+                rec_count: { $size: "$recipients" }
+            }
+        },
+        {
+            $sort: { rec_count : -1}
+        },
+        {
+            $limit: 1
+        }
+    ]
+)
 
 //4. Wieviel E-Mails enthalten genau einen Sender und genau einen Rezipienten?
 db.mail.count({
@@ -34,5 +48,6 @@ db.mail.count({
     date: {
         $gte: "2001-01-01T00:00:00.000Z",
         $lt: "2002-01-01T00:00:00.000Z"
-    }
+    },
+    folder : "_sent"
 })
