dfe24b3435e380b82ad815e013fa400b7cbec131
Marv <marvin.lutz@fh-bielefeld.de>
2015-05-26 15:47:36 +0200

    server seite

--- node_serv/app.js
+++ node_serv/app.js
@@ -31,6 +31,13 @@ app.use(methodOverride('_method'));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    next();
+});
+
 app.use('/', routes);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- node_serv/routes/index.js
+++ node_serv/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mail = require('../model/mail');
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {title: 'Mail List'});
@@ -19,9 +20,22 @@ router.get('/folder', function (req, res, next) {
         ); // end aggregate
 });
 
+router.delete('/folder/delete/:name', function(req,res,next){
+    console.log("delete folder: " + req.params.name);
+    var name = req.params.name;
+    if (name == "null") name = null;
+
+    mail.find({folder: name}).remove(
+        function (err) {
+            if (err) throw err;
+
+            res.send(JSON.stringify({ status: "deleted" }));
+            //res.redirect("/folder/");
+        }); //end remove
+});
 
 /*folder by name route*/
-router.route('/folder/:name/')
+router.route('/folder/:name/:newname')
     /* GET message of folder */
     .get(function (req, res, next) {
             console.log("show all messagses in folder: " + req.params.name);
@@ -29,7 +43,7 @@ router.route('/folder/:name/')
             mail.aggregate(
                 [
                     {$match: {folder: name}},
-                    {$group: {_id: "$_id", _text: {$push: "$text"}}}
+                    {$group: {_id: "$_id", _text: {$push: "$text"}, _subject: {$push: "$subject"}}}
                 ], function (err, data) {
                     if (err) throw err;
 
@@ -67,7 +81,7 @@ router.route('/folder/:name/')
     .put(function (req, res, next) {
         console.log("update folder: " + req.params.name);
         mail.update({folder: req.params.name},
-            {$set: {folder: req.body.changed}},
+            {$set: {folder: req.params.newname}},
             {multi: true},
             function (err, data) {
                 if (err) throw err;
@@ -105,6 +119,18 @@ router.route('/newMessage')
         });
     });
 
+router.delete('/folder/:name/message/:id/delete', function (req, res, next) {
+    console.log("delete: " + req.params.id);
+
+    mail.find({_id: req.params.id}).remove(
+        function (err) {
+            if (err) throw err;
+
+            res.send(JSON.stringify({ status: "deleted" }));
+            //res.redirect("/folder/" + req.params.name);
+        }); //end remove
+});
+
 router.route('/folder/:name/message/:id')
     /* READ specific message */
     .get(function (req, res, next) {
@@ -114,7 +140,7 @@ router.route('/folder/:name/message/:id')
         mail.aggregate(
             [
                 {$match: {folder: name}},
-                {$group: {_id: "$_id", _text: {$push: "$text"}, _folder: {$push: "$folder"}}}
+                {$group: {_id: "$_id", _text: {$push: "$text"}, _folder: {$push: "$folder"}, _date: {$push: "$date"}, _subject: {$push: "$subject"}, _sender: {$push: "$sender"}}}
             ], function (err, data) {
                 if (err) throw err;
                 if (id < data.length) {
@@ -135,63 +161,4 @@ router.route('/folder/:name/message/:id')
         );//end aggregate
     });
 
-/*message by id route*/
-router.route('/folder/:name/message/:id')
-    /* READ specific message*/
-    .get(function (req, res, next) {
-        console.log("read message: " + req.params.id);
-        var name = req.params.name;
-        var id = req.params.id;
-        mail.aggregate(
-            [
-                {$match: {folder: name}},
-                {$group: {_id: "$_id", _text: {$push: "$text"}, _folder: {$push: "$folder"}}}
-            ], function (err, data) {
-                if (err) throw err;
-                if (id < data.length) {
-                    mail.aggregate(
-                        [
-                            {$group: {_id: "$folder"}}
-                        ], function (err, folder) {
-
-                            res.json(data[id]);
-                            /*res.render('./new/readmessage', {
-                                folder: name,
-                                data: data[id],
-                                oldId: id,
-                                folders: folder
-                            });*/
-                        }
-                    ); // end aggregate
-                } else {
-                    res.send(JSON.stringify({ status: "not found" }));
-                    //res.render("index", {title: "There is no message"});
-                }
-
-            }
-        );//end aggregate
-    })
-    /*DELETE message */
-    .delete(function (req, res, next) {
-        console.log("delete: " + req.params.id);
-
-        mail.find({_id: req.params.id}).remove(
-            function (err) {
-                if (err) throw err;
-
-                res.send(JSON.stringify({ status: "deleted" }));
-                //res.redirect("/folder/" + req.params.name);
-            }); //end remove
-    })
-    /*Move existing message */
-    .put(function (req, res, next) {
-        mail.findOne({_id: req.params.id},
-            function (err, doc) {
-                if (err) throw err;
-                doc.folder = req.body.chose;
-                doc.save();
-                res.json(doc);
-                //res.redirect("/folder/" + req.body.chose);
-            });
-    });
 module.exports = router;
