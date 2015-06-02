6e25e46d218b32bad8a00f49b4c48453b4f1d1de
Marv <marvin.lutz@fh-bielefeld.de>
2015-06-02 13:49:05 +0200

    folder init
    $http added as lib

--- angular2/src/folder.js
+++ angular2/src/folder.js
@@ -1,17 +1,122 @@
 import {ComponentAnnotation as Component, ViewAnnotation as View} from 'angular2/angular2';
+import {NgFor}  from 'angular2/angular2';
+//$http Var
+import {$http} from 'xhr-factory';
 
 @Component({
     selector: 'folder'
 })
 
 @View({
-    templateUrl: 'folder.html'
+    templateUrl: "folder.html",
+    directives: [NgFor]
 })
 
 export class Folder {
 
+    //folderList: array;
+
     constructor() {
-        console.info('Folder Component Mounted Successfully');
+        console.log('Folder.Constructor()');
+        //Vars
+        this.folderList = ['a','b','c'];
+        this.folderLoaded = false;
+        this.htmlName = "Hallo Welt!";
+        //Init
+        this.getFolder();
+
+        this.deleteFolder("HONK KONG");
+    }
+
+    getFolder() {
+        var debug_name = 'Folder.getFolder()';
+        console.log(debug_name);
+
+        $http.get('http://localhost:3000/folder')
+            .then((data) => {
+                console.log(debug_name + ' Sucessfull!');
+                this.folderList = data;
+                console.log(this.folderList);
+            })
+            .catch((error) => {
+                alert(debug_name + ' Error!');
+            });
+    }
+
+    deleteFolder(folderName) {
+        var debug_name = 'Folder.deleteFolder(\"' + folderName + '\")';
+        console.log(debug_name);
+        if(!this.folderLoaded){
+            console.log('foalderList not laoded yet...');
+            return;
+        }
+
+        $http.delete('http://localhost:3000/folder/delete/' + folderName).
+            then((data) => {
+                console.log(debug_name + ' Successfull!');
+                this.removeFromList(folderName);
+            })
+            .catch((error) => {
+                alert(debug_name + ' Error!');
+            });
+    }
+
+    clickRemove(name){
+        var debug_name = 'Folder.clickRemove(\"' + name + '\")';
+        console.log(debug_name);
+    }
+
+    removeFromList(name){
+        var debug_name = 'Folder.removeFromList(\"' + name + '\")';
+        console.log(debug_name);
+
+        for(var i = 0; i < this.folderList.length; i++){
+            if(this.folderList[i]._id == name){
+                console.log("Removed from folderList -> " + name);
+                this.folderList.splice(i, 1);
+                return;
+            }
+        }
+        console.log("Nothing removed from folderList.")
     }
 
 }
+
+/**
+ WICHTIG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ https://github.com/angular/angular/issues/1655
+
+ I found the issue regarding the directives. The documentation refers to the For, If and Switch directives, but when I dived into the code, I found it these directives actually are NgFor, NgIf and NgSwitch. Not sure why, probably for backward compatibility with angular 1.4?
+
+ When keeping that in mind, and using this code as the main.ts file
+
+ import {Component, View, NgFor, bootstrap} from "angular2/angular2";
+
+ @Component({
+  selector: 'my-app',
+  appInjector: [FooService]
+})
+ @View({
+  template: `
+    <h1>Sup {{ name }}</h1>
+    <div *ng-for="#item of list">{{ item }}</div>
+  `,
+  directives: [NgFor]
+})
+ class AppComponent {
+
+  name: string;
+  list: string[];
+
+  constructor(fooService: FooService) {
+    this.name = 'Ben';
+    this.list = fooService.list;
+  }
+}
+
+ bootstrap(AppComponent);
+ Everything should work like a charm.
+
+ Note: Do not use the angular2.d.ts type definition because it will not compile. The type definition uses For, If and Switch as well.
+
+ */
