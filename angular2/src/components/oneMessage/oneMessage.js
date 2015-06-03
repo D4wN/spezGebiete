5dd1b671e5a3b853ae78bc4ee5c60065ee827a1f
Marv <marvin.lutz@fh-bielefeld.de>
2015-06-03 13:41:13 +0200

    changed loading buttons and detail for oneMessage

--- angular2/src/components/oneMessage/oneMessage.js
+++ angular2/src/components/oneMessage/oneMessage.js
@@ -5,48 +5,52 @@ import {NgFor}  from 'angular2/angular2';
 import {$http} from '../xhr-factory';
 
 @Component({
-    selector: 'oneMessage',
-    properties: {
-        'mid': 'mid',
-        'fid': 'fid'
-    }
+  selector: 'oneMessage',
+  properties: {
+    'mid': 'mid',
+    'fid': 'fid'
+  }
 })
 
 @View({
-    templateUrl: System.baseURL + "components/oneMessage/oneMessage.html",
-    directives: [NgFor]
+  templateUrl: System.baseURL + "components/oneMessage/oneMessage.html",
+  directives: [NgFor]
 })
 
 export class OneMessage {
 
-    constructor() {
-        console.log('OneMessage.Constructor()');
+  constructor() {
+    console.log('OneMessage.Constructor()');
 
-        this.mid = "mid_default";
-        this.fid = "fid_default";
+    this.mid = "mid_default";
+    this.fid = "fid_default";
+    this.hidden = true;
 
-        this.m = [];
+    this.m = [];
+  }
+
+  getMessage() {
+    var debug_name = 'OneMessage.getMessage()';
+    console.log(debug_name);
+
+    $http.get('http://localhost:3000/folder/' + this.fid + '/message/' + this.mid)
+      .then((data) => {
+        console.log(debug_name + ' Sucessfull!');
+        this.m[0] = data;
+        console.log(this.m);
+      })
+      .catch((error) => {
+        alert(debug_name + ' Error!');
+      });
+  }
+
+  clickDetail() {
+    var debug_name = 'OneMessage.clickDetail()';
+    console.log(debug_name);
+
+    this.hidden = !this.hidden;
+    if(!this.hidden){
+      this.getMessage()
     }
-
-    getMessage(){
-        var debug_name = 'OneMessage.getMessage()';
-        console.log(debug_name);
-
-        $http.get('http://localhost:3000/folder/'+ this.fid +'/message/' + this.mid)
-            .then((data) => {
-                console.log(debug_name + ' Sucessfull!');
-                this.m[0] = data;
-                console.log(this.m);
-            })
-            .catch((error) => {
-                alert(debug_name + ' Error!');
-            });
-    }
-
-    clickLoad(){
-        var debug_name = 'OneMessage.clickLoad()';
-        console.log(debug_name);
-
-        this.getMessage();
-    }
+  }
 }
