4178a9d6ff04acacc3b8c70c4647b7a5b38b534d
Marv <marvin.lutz@fh-bielefeld.de>
2015-06-09 12:46:24 +0200

    nr2 init push

---
+++ angular_client_metal/public/app.js
@@ -0,0 +1,178 @@
+var myApp = angular.module('myApp', ['ngRoute', 'ui.bootstrap'])
+    .controller('myAppMessageController', function($scope, $http, $route, $routeParams){
+        $scope.folderName = undefined;
+
+        //DELETE
+        $scope.deleteMessage = function(val){
+            $http.delete('http://localhost:3000/folder/'+ $scope.folderName +'/message/'+ val +'/delete').
+                success(function(data, status, headers, config) {
+                    console.log("folderCtrl.delete Success!");
+                    //$route.reload();
+                    $scope.getMessages();
+                }).
+                error(function(data, status, headers, config) {
+                    alert("folderCtrl.delete Error!");
+                });
+        }
+
+        //SHOW ALL MESSAGES FROM FOLDER
+        $scope.getMessages = function(){
+            if($scope.folderName === undefined){
+                alert("folderName undefined");
+                return;
+            }
+
+            $http.get('http://localhost:3000/folder/'+ $scope.folderName +'/message').
+                success(function(data, status, headers, config) {
+                    console.log("Success! Messages");
+                    console.log(data);
+                    $scope.messageList = data;
+                }).
+                error(function(data, status, headers, config) {
+                    alert("Error!");
+                });
+        }
+        //MOVE
+        $scope.moveMessage = function(mId, newFolder){
+            console.log('http://localhost:3000/folder/'+ $scope.folderName +'/message/'+ mId + '    TO: ' + newFolder);
+            alert("SIEHE TODO!");
+            //TODO move funktioniert nicht ->  No 'Access-Control-Allow-Origin' header is present on the requested resource.
+            return; //FIXME @ NICLAS
+            $http.put('http://localhost:3000/folder/'+ $scope.folderName +'/message/'+ mId, newFolder).
+                success(function(data, status, headers, config) {
+                    console.log("move Folder Success!");
+                    //$location.path('folder/'+ $scope.chose._id);
+                    $scope.getMessages();
+                }).
+                error(function(data, status, headers, config) {
+                    alert("move Folder Error!");
+                });
+        };
+
+        $scope.getFolder = function(){
+            $http.get('http://localhost:3000/folder/').
+                success(function(data, status, headers, config) {
+                    console.log("Success! Folder");
+                    console.log(data);
+                    $scope.folderList = data;
+                    $scope.chose = $scope.folderList[0];
+                }).
+                error(function(data, status, headers, config) {
+                    alert("Error!");
+                });
+        }
+
+        //WATCHER FOR MESSAGES
+        $scope.$watch('folderName', function(){
+            if(!$scope.isCollapsed && $scope.folderName != undefined){
+                $scope.getMessages();
+                $scope.getFolder();
+            }
+        });
+
+        $scope.$watch('isCollapsed', function(){
+            if(!$scope.isCollapsed && $scope.folderName != undefined){
+                $scope.getMessages();
+                $scope.getFolder();
+            }
+        });
+    })
+    .controller('myAppOneMessageController', function($scope, $http, $route, $routeParams, $location){
+        $scope.parentFolder = undefined;
+        $scope.mId = undefined;
+
+        //DELETE
+        /*$scope.deleteMessage = function(){
+            $http.delete('http://localhost:3000/folder/'+ $scope.parentFolder +'/message/'+ $scope.msg._id +'/delete').
+                success(function(data, status, headers, config) {
+                    console.log("folderCtrl.delete Success!");
+                    $location.path('/folder/'+ $scope.parentFolder);
+                }).
+                error(function(data, status, headers, config) {
+                    alert("folderCtrl.delete Error!");
+                });
+        };*/
+
+        //GET MESSAGE
+        $scope.getMessageDetails = function(){
+            $http.get('http://localhost:3000/folder/'+ $scope.parentFolder +'/message/'+ $scope.mId).
+                success(function(data, status, headers, config) {
+                    console.log("Success! Messages");
+                    console.log(data);
+                    $scope.msg = data;
+                }).
+                error(function(data, status, headers, config) {
+                    alert("Error!");
+                });
+        }
+
+        //WATCHER FOR MESSAGES
+        $scope.$watch('parentFolder', function(){
+            if(!$scope.isCollapsed && $scope.parentFolder != undefined && $scope.mId != undefined){
+                $scope.getMessageDetails();
+            }
+        });
+
+        $scope.$watch('isCollapsed', function(){
+            if(!$scope.isCollapsed && $scope.parentFolder != undefined && $scope.mId != undefined){
+                $scope.getMessageDetails();
+            }
+        });
+
+        //init, get folder list
+        $http.get('http://localhost:3000/folder/').
+            success(function(data, status, headers, config) {
+                console.log("Success! Folder");
+                console.log(data);
+                $scope.folderList = data;
+                $scope.chose = $scope.folderList[0];
+
+            }).
+            error(function(data, status, headers, config) {
+                alert("Error!");
+            });
+    })
+    .controller('NewMessageCtrl', function($scope, $http, $modalInstance){
+        $scope.form = {};
+
+        /*$http.get('http://localhost:3000/folder/').
+            success(function(data) {
+                $scope.data = data;
+            });*/
+
+        $scope.ok = function (){
+            console.log("OK MODAL");
+            $modalInstance.close($scope.form);
+        };
+
+        $scope.cancel = function (){
+            console.log("cancel MODAL");
+            $modalInstance.dismiss('cancel');
+        };
+    })
+    .config(['$routeProvider', function($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'folder.html',
+                controller: folderCtrl
+            })
+            .when('/folder',{
+                templateUrl: 'folder.html',
+                controller: folderCtrl
+            })
+            .when('/folder/:name',{
+                templateUrl: 'messages.html',
+                controller: messageCtrl
+            })
+            .when('/folder/:name/:msgId',{
+                templateUrl: 'oneMessage.html',
+                controller: oneMessageCtrl
+            })
+            .when('/newMessage',{
+                templateUrl: 'createmessage.html',
+                controller: NewMessageCtrl
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }]);
---
+++ angular_client_metal/public/controllers.js
@@ -0,0 +1,193 @@
+var IndexCtrl = function($scope, $http) {
+    console.log("index route");
+    /*$http.get('localhost:3000').
+        success(function(data, status, headers, config) {
+            console.log("data:");
+            console.log(data);
+            $scope.posts = data.posts;
+        });*/
+}
+
+var folderCtrl = function($scope, $http, $route, $modal){
+    console.log("Get Folder...");
+    $scope.deleteFolder = function(val){
+        $http.delete('http://localhost:3000/folder/delete/'+val).
+            success(function(data, status, headers, config) {
+                console.log("folderCtrl.delete Success!");
+                //$route.reload();
+                $scope.getFolder();
+            }).
+            error(function(data, status, headers, config) {
+                alert("folderCtrl.delete Error!");
+            });
+    }
+    $scope.renameFolder = function(val, newName){
+        if(newName === undefined) return;
+        if(newName == val) return;
+
+        $http.put('http://localhost:3000/folder/'+val+'/'+newName).
+            success(function(data, status, headers, config) {
+                console.log("renameFolder.delete Success!");
+                //$route.reload();
+                $scope.getFolder();
+            }).
+            error(function(data, status, headers, config) {
+                alert("renameFolder.delete Error!");
+            });
+    }
+
+    $scope.getFolder = function(){
+        $http.get('http://localhost:3000/folder').
+            success(function(data, status, headers, config) {
+                console.log("Success!");
+                console.log(data);
+                $scope.folderList = data;
+            }).
+            error(function(data, status, headers, config) {
+                alert("Error!");
+            });
+    }
+
+
+    $scope.newMessageData = undefined;
+    //MODAL NEW MESSAGE DIALOG
+    $scope.newMessage = function(){
+        var modalInstance = $modal.open({
+            animation: true,
+            templateUrl: 'myModalContent.html',
+            controller: 'NewMessageCtrl',
+            //size: size,
+            resolve: {
+                newMessageData: function () {
+                    return $scope.form;
+                }
+            }
+        });
+
+        modalInstance.result.then(function (data) {
+            $scope.createNewMessage(data);
+        }, function () {
+            console.log('Modal dismissed at: ' + new Date());
+        });
+    };
+
+    $scope.createNewMessage = function (data) {
+        if(data == null || data === undefined) return;
+        $http.post('http://localhost:3000/newMessage', data).
+            success(function(data) {
+                //$location.path('/');
+                console.log("SUCCESS! -> new MEssage submitPost");
+                $scope.getFolder();
+            });
+    };
+
+    //init
+    $scope.getFolder();
+}
+
+//MESSAGE
+var messageCtrl = function($scope, $http, $route, $routeParams){
+    $scope.folderName = $routeParams.name;
+
+    //DELETE
+    $scope.deleteMessage = function(val){
+        $http.delete('http://localhost:3000/folder/'+ $scope.folderName +'/message/'+ val +'/delete').
+            success(function(data, status, headers, config) {
+                console.log("folderCtrl.delete Success!");
+                $route.reload();
+            }).
+            error(function(data, status, headers, config) {
+                alert("folderCtrl.delete Error!");
+            });
+    }
+
+    //SHOW ALL MESSAGES FROM FOLDER X
+    $http.get('http://localhost:3000/folder/'+ $scope.folderName +'/message').
+        success(function(data, status, headers, config) {
+            console.log("Success! Messages");
+            console.log(data);
+            $scope.messageList = data;
+        }).
+        error(function(data, status, headers, config) {
+            alert("Error!");
+        });
+}
+
+//ONE MESSAGE
+var oneMessageCtrl = function($scope, $http, $route, $routeParams, $location, $modal){
+    $scope.folderName = $routeParams.name;
+
+    //DELETE
+    $scope.deleteMessage = function(){
+        $http.delete('http://localhost:3000/folder/'+ $scope.folderName +'/message/'+ $scope.msg._id +'/delete').
+            success(function(data, status, headers, config) {
+                console.log("folderCtrl.delete Success!");
+                $location.path('/folder/'+ $scope.folderName);
+            }).
+            error(function(data, status, headers, config) {
+                alert("folderCtrl.delete Error!");
+            });
+    };
+    //MOVE
+    $scope.moveMessage = function(){
+        console.log('http://localhost:3000/folder/'+ $scope.chose._id);
+        $http.put('http://localhost:3000/folder/'+ $scope.folderName +'/message/'+ $scope.msg._id, $scope.chose).
+            success(function(data, status, headers, config) {
+                console.log("move Folder Success!");
+                $location.path('folder/'+ $scope.chose._id);
+            }).
+            error(function(data, status, headers, config) {
+                alert("move Folder Error!");
+            });
+    };
+
+    $http.get('http://localhost:3000/folder/').
+        success(function(data, status, headers, config) {
+            console.log("Success! Folder");
+            console.log(data);
+            $scope.folderList = data;
+            $scope.chose = $scope.folderList[0];
+
+        }).
+        error(function(data, status, headers, config) {
+            alert("Error!");
+        });
+
+    $http.get('http://localhost:3000/folder/'+ $scope.folderName +'/message/'+ $routeParams.msgId).
+        success(function(data, status, headers, config) {
+            console.log("Success! Messages");
+            console.log(data);
+            $scope.msg = data;
+        }).
+        error(function(data, status, headers, config) {
+            alert("Error!");
+        });
+}
+
+//NEW MESSAGE
+var NewMessageCtrl = function($scope, $http, $location, $routeParams){
+    $scope.form = {};
+
+    $http.get('http://localhost:3000/folder/').
+        success(function(data) {
+            $scope.data = data;
+        });
+
+    $scope.submitPost = function () {
+        console.log($scope.form);
+        $http.post('http://localhost:3000/newMessage', $scope.form).
+            success(function(data) {
+                $location.path('/');
+            });
+    };
+
+    $scope.ok = function (){
+        console.log("OK MODAL");
+        $modalInstance.close($scope.selected.item);
+    };
+
+    $scope.cancel = function (){
+        console.log("cancel MODAL");
+        $modalInstance.dismiss('cancel');
+    };
+}
