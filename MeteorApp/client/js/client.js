3298e2212cb32793f47728c6f7431b378123bfef
Marv <marvin.lutz@fh-bielefeld.de>
2015-06-29 16:30:32 +0200

    more stuff
    - rename/delete only dummy

--- MeteorApp/client/js/client.js
+++ MeteorApp/client/js/client.js
@@ -1,60 +1,74 @@
-if (Meteor.isClient) {
-    console.log("CLIENT");
+Template.body.helpers({
+    //TODO
+    Folder: function () {
+        //Meteor.get('folderList');
+        /*var folder = {
+         TEST: "TEST WELT",
+         list: [
+         {_id: "Folder 1"},
+         {_id: "MYFolder"},
+         {_id: "Rolands Folder"}]
+         };*/
 
-    Template.body.helpers({
-        //TODO
-        Folder: function () {
-            //Meteor.get('folderList');
-            var list = [
-                {_id: "Folder 1"},
-                {_id: "MYFolder"},
-                {_id: "Rolands Folder"}
-            ]
-            console.log(list);
+        var list = [
+            {_id: "Folder 1"},
+            {_id: "MYFolder"},
+            {_id: "Rolands Folder"}
+        ]
+        console.log(list);
 
-            return list;
-        },
-        MessageList: function(){
-            var list = [
-                {_id: "Folder 1"},
-                {_id: "MYFolder"},
-                {_id: "Rolands Folder"}
-            ]
+        return list;
+    }
+});
+
+Template.folder.helpers({
+    hideFolderDiv: function () {
+        var key = 'folder_' + this._id + 'show';
+        if (Session.get('folder_' + this._id + 'show')) {
+            //console.log("ID(TRUE)= " + this._id);
+            return true;
+        } else {
+            //console.log("ID(FALSE)= " + this._id);
+            return false;
         }
-    });
+    },
+    MessageList: function () {
+        console.log("ms liste");
+        var list = [
+            {_id: "m 1"},
+            {_id: "m2 "},
+            {_id: "m3 "}
+        ];
 
-    Template.folder.helpers({
-        hideFolderDiv: function () {
-            var key = 'folder_' + this._id + 'show';
+        return list;
+    }
+});
 
+Template.folder.events({
+    "click .hideButtonFolder": function (event) {
+        //console.log("clicked! " + this._id);
 
-            if (Session.get('folder_' + this._id + 'show')) {
-                console.log("ID(TRUE)= " + this._id);
-                return true;
-            } else {
-                console.log("ID(FALSE)= " + this._id);
-                return false;
-            }
+        var key = 'folder_' + this._id + 'show';
+        if (Session.get(key)) {
+            Session.set(key, false);
+        } else {
+            Session.set(key, true);
         }
-    });
+    },
+    "click .removeFolder": function (event) {
+        console.log("Remove Folder " + this._id);
+        //Folder.remove(this._id);
+    },
+    "submit .renameFolderForm": function (event) {
+        var text = event.target.text.value;
+        if(text === undefined || text == null || text == "")
+            return false;
 
-    Template.folder.events({
-        "click .hideButtonFolder": function (event) {
-            //console.log("clicked! " + this._id);
+        console.log("Rename " + this._id + " to " + text);
 
-            var key = 'folder_' + this._id + 'show';
-            if (Session.get(key)) {
-                Session.set(key, false);
-            } else {
-                Session.set(key, true);
-            }
-        }
-    });
-
-} else {
-    //Server
-    console.log("SERVER");
-}
-
+        event.target.text.value = "";
+        return false;
+    }
+});
 
 
